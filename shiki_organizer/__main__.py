7e1346d498bf0caf7f6692d25f3ce5f669232d9e
Niko Honu <nikohonu@gmail.com>
2022-11-14 01:23:44 +0200

    add ability to do tasks from gui

--- shiki_organizer/__main__.py
+++ shiki_organizer/__main__.py
@@ -2,35 +2,62 @@ import random
 import sys
 
 from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtWidgets import QCheckBox, QHBoxLayout, QPushButton, QVBoxLayout, QWidget
 
 from shiki_organizer.task import TaskTableModel
+
+
+class TasksWidegt(QWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+        self.main_layout = QVBoxLayout()
+        self.header_layout = QHBoxLayout()
+        self.footer_layout = QHBoxLayout()
+
+        self.view = QtWidgets.QTableView()
+        self.model = TaskTableModel()
+        self.view.setModel(self.model)
+        self.today = QCheckBox("Show only today task")
+        self.do_button = QPushButton("Do")
+        self.today.stateChanged.connect(self.toggle_today)
+        self.do_button.clicked.connect(self.do)
+
+        self.header_layout.addWidget(self.today)
+        self.footer_layout.addWidget(self.do_button)
+        self.main_layout.addLayout(self.header_layout)
+        self.main_layout.addWidget(self.view)
+        self.main_layout.addLayout(self.footer_layout)
+        self.setLayout(self.main_layout)
+
+    def toggle_today(self, state):
+        self.model.toggle_today(state)
+
+    def do(self):
+        self.model.do(self.view.selectionModel().selectedIndexes())
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.resize(900, 500)
-        self.view = QtWidgets.QTableView()
-        self.model = TaskTableModel()
-        self.view.setModel(self.model)
-        self.setCentralWidget(self.view)
+        self.setCentralWidget(TasksWidegt())
 
-        #self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        # self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
 
-        #self.button = QtWidgets.QPushButton("Click me!")
-        #self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        # self.button = QtWidgets.QPushButton("Click me!")
+        # self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
 
-        #self.layout = QtWidgets.QVBoxLayout(self)
+        # self.layout = QtWidgets.QVBoxLayout(self)
         # self.setCentralWidget(self.layout)
-        #self.layout.addWidget(self.text)
-        #self.layout.addWidget(self.button)
+        # self.layout.addWidget(self.text)
+        # self.layout.addWidget(self.button)
 
-        #self.button.clicked.connect(self.magic)
-        #self.setCentralWidget(self.button)
+        # self.button.clicked.connect(self.magic)
+        # self.setCentralWidget(self.button)
 
-    #@QtCore.Slot()
-    #def magic(self):
-        #self.text.setText(random.choice(self.hello))
+    # @QtCore.Slot()
+    # def magic(self):
+    # self.text.setText(random.choice(self.hello))
 
 
 def main():
--- shiki_organizer/task.py
+++ shiki_organizer/task.py
@@ -1,7 +1,10 @@
-from PySide6.QtCore import QAbstractListModel, QAbstractTableModel, QModelIndex, Qt
+from datetime import date, datetime, timedelta
+
+from PySide6.QtCore import QAbstractTableModel, QModelIndex, Qt, Signal
 from PySide6.QtGui import QBrush, QColor
 from PySide6.QtWidgets import QLabel
 
+from shiki_organizer.commands.stop_command import run_stop_command
 from shiki_organizer.model import Task
 
 
@@ -9,22 +12,57 @@ class TaskTableModel(QAbstractTableModel):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._tasks = [task for task in Task.select()]
+        self._headers = ["Name", "Recurrence", "Due"]
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int):
+        if orientation == Qt.Orientation.Horizontal and role == Qt.DisplayRole:
+            return self._headers[section]
+        if orientation == Qt.Orientation.Vertical and role == Qt.DisplayRole:
+            return self._tasks[section].id
+        return super().headerData(section, orientation, role)
 
     def data(self, index: QModelIndex, role: int):
-        print(role)
         if role == Qt.DisplayRole:
-            if index.column() == 0:
-                return self._tasks[index.row()].id
-            elif index.column() == 1:
-                return self._tasks[index.row()].description
-            elif index.column() == 2:
-                if self._tasks[index.row()].recurrence:
-                    return str(self._tasks[index.row()].recurrence) + "d"
-            else:
-                return str(self._tasks[index.row()].scheduled)
+            match index.column():
+                case 0:
+                    return self._tasks[index.row()].description
+                case 1:
+                    if self._tasks[index.row()].recurrence:
+                        return str(self._tasks[index.row()].recurrence) + "d"
+                case _:
+                    return str(self._tasks[index.row()].scheduled)
 
     def rowCount(self, parent: QModelIndex) -> int:
         return len(self._tasks)
 
     def columnCount(self, parent: QModelIndex) -> int:
-        return 4
+        return 3
+
+    def refresh(self):
+        self.dataChanged.emit(1, 1)
+        self.headerDataChanged.emit(Qt.Orientation.Vertical, 1, 1)
+
+    def toggle_today(self, state):
+        if state:
+            self._tasks = [
+                task for task in Task.select().where(Task.scheduled == date.today())
+            ]
+        else:
+            self._tasks = [task for task in Task.select()]
+        self.refresh()
+
+    def do(self, indexes: list):
+        for row in set([index.row() for index in indexes]):
+            task = self._tasks[row]
+            if task.recurrence:
+                task.scheduled = (
+                    datetime.now() + timedelta(days=task.recurrence)
+                ).date()
+                if task.deadline and task.scheduled > task.deadline:
+                    task.is_completed = True
+                    task.scheduled = datetime.now()
+            else:
+                task.is_completed = True
+            task.save()
+            run_stop_command()
+        self.refresh()
